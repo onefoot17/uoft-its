1688176d147a4499c6fc857671e9fdadbeed2b54
Earl Misquitta <e.misquitta@utoronto.ca>
2019-11-04 15:34:57 -0500

    Changing Font Awesome from CDN to local and separating nav into it's own template

--- wp-content/themes/2019_its/build/header.php
+++ wp-content/themes/2019_its/build/header.php
@@ -24,29 +24,5 @@
                 </a>
             </section>
 
-            <nav class="nav">
-                <section class="nav__top">
-                    <a class="nav__esc" href="https://uthrprod.service-now.com/help">
-                        <section class="nav__its-alerts__text">Enterprise Service Centre</section>
-                    </a>
-
-                    <a class="nav__its-alerts" href="http://www.systemstatus.utoronto.ca/">
-                        <section class="nav__its-alerts__icon"></section>
-                        
-                        <section class="nav__its-alerts__text">ITS Alerts</section>
-                    </a>
-
-                    <?php wp_nav_menu( array(
-                        'theme_location' => 'social-links',
-                        'container' => '',
-                        'menu_class' => 'nav__menu nav__menu--social'
-                    ) ); ?>
-                </section>
-                
-                <?php wp_nav_menu( array(
-                    'theme_location' => 'main-menu',
-                    'container' => '',
-                    'menu_class' => 'nav__menu nav__menu--main nav__bottom'
-                ) ); ?>
-            </nav>
+            <?php get_template_part( 'nav' ); ?>
         </header>
