adaf753daf3cc6f1e50bda23f121e58bf7fca57c
Earl Misquitta <e.misquitta@utoronto.ca>
2019-11-05 15:43:52 -0500

    Nav merge conflcits

--- wp-content/themes/2019_its/build/nav.php
+++ wp-content/themes/2019_its/build/nav.php
@@ -14,12 +14,29 @@
             'theme_location' => 'social-links',
             'container' => '',
             'menu_class' => 'nav__menu nav__menu--social'
-        ) ); ?>
+            ) ); ?>
     </section>
+
+    <section class="nav__bottom">
+        <a href="#menu-main" id="menu-main-toggle" class="menu-toggle" aria-label="Open main menu">
+            <span class="sr-only">Open main menu</span>
+
+            <span class="fa fa-bars" aria-hidden="true"></span>
+        </a>
+
+        <section id="menu-main" class="menu-main">
+            <a href="#menu-main-toggle" id="menu-main-close" class="menu-close" aria-label="Close main menu">
+                <span class="sr-only">Close main menu</span>
     
-    <?php wp_nav_menu( array(
-        'theme_location' => 'main-menu',
-        'container' => '',
-        'menu_class' => 'nav__menu nav__menu--main nav__bottom'
-    ) ); ?>
+                <span class="fa fa-close" aria-hidden="true"></span>
+            </a>
+
+            <?php wp_nav_menu( array(
+                'theme_location' => 'main-menu',
+                'container' => '',
+                'menu_class' => 'nav__menu nav__menu--main',
+                'items_wrap' => '<ul class="%2$s" role="menubar" aria-label="Main menu">%3$s</ul>'
+            ) ); ?>
+        </section>
+    </section>
 </nav>
