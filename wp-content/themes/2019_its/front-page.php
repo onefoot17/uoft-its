c4fbd2d5a47a101cf7b9c385cad4ba209f3a1230
Earl Misquitta <e.misquitta@utoronto.ca>
2019-11-05 15:31:53 -0500

    Changes  from Carrie, Follow section option 1, remove unneeded SASS folder

--- wp-content/themes/2019_its/front-page.php
+++ wp-content/themes/2019_its/front-page.php
@@ -144,87 +144,93 @@
         </section>
     </section>
 
-    <section class="section section--follow">
-        <section class="section__width section--follow__width">
-            <header class="section__header">
-                <h1 class="section--follow__heading">Follow Us</h1>
-            </header>
-
-            <ul class="section__posts section--follow__posts">
-                <?php
-                    global $post;
-
-                    $args = array(
-                        'category_name' => 'Follow',
-                        'posts_per_page' => 4
-                    );
-
-                    $postslist = get_posts( $args );
-
-                    foreach ( $postslist as $post ) :
-                        setup_postdata( $post ); 
-                ?>
-                            
-                        <li class="section--follow__post">
-                            <a class="section__link section--follow__link" href="<?php the_permalink(); ?>">
-                                <button class="section--follow__button section--follow__link__button"><?php the_title(); ?></button>
-                            </a>
-                        </li>
-                <?php
-                    endforeach;
-
-                    wp_reset_postdata();
-                ?>
-            </ul>
-        </section>
-    </section>
-
     <section class="section section--highlights">
         <section class="section__width section--highlights__width">
-            <ul class="section__posts section--highlights__posts">
-                <?php
-                    global $post;
+            <header class="section__header section--highlights__header">
+                <h1 class="section--highlights__heading">Follow Us</h1>
+            </header>
 
-                    $args = array(
-                        'category_name' => 'Highlights',
-                        'posts_per_page' => 4
-                    );
+            <section class="section--highlights__postsCon">
+                <ul class="section__posts section--highlights__posts">
+                    <?php
+                        global $post;
 
-                    $postslist = get_posts( $args );
+                        $args = array(
+                            'category_name' => 'Highlights',
+                            'posts_per_page' => 4
+                        );
 
-                    foreach ( $postslist as $post ) :
-                        setup_postdata( $post ); 
-                ?>
-                            
-                        <li class="section--highlights__post">
-                            <a class="section__link section--highlights__link" href="<?php the_permalink(); ?>" target="_blank">
-                                <header class="section--highlights__link__top">
-                                    <h2 class="section__link__title section--highlights__link__title"><?php the_title(); ?></h2>
+                        $postslist = get_posts( $args );
 
-                                    <button class="section--highlights__button section--highlights__link__button"><?php the_field('read_more_button'); ?></button>
-                                </header>
+                        foreach ( $postslist as $post ) :
+                            setup_postdata( $post ); 
+                    ?>
+                                
+                            <li class="section--highlights__post">
+                                <a class="section__link section--highlights__link" href="<?php the_permalink(); ?>" target="_blank">
+                                    <header class="section--highlights__link__top">
+                                        <h2 class="section__link__title section--highlights__link__title"><?php the_title(); ?></h2>
 
-                                <section class="section--highlights__link__bottom">
-                                    <?php 
-                                        $turn_off_excerpt = get_field('turn_off_excerpt');
+                                        <button class="section--highlights__button section--highlights__link__button"><?php the_field('read_more_button'); ?></button>
+                                    </header>
 
-                                        if( ! $turn_off_excerpt ) {
-                                            echo '<p class="section--highlights__link__excerpt">' . get_the_excerpt();
-                                        }
-                                        
-                                        if ( has_post_thumbnail() ) {
-                                            echo '<section class="section--highlights__link__image-con">' . wp_get_attachment_image(get_post_thumbnail_id(), 'medium', false, array('class' => 'section__link__image section--highlights__link__image') ) . '</section>';
-                                        }
-                                    ?>
-                                </section>
-                            </a>
-                        </li>
-                <?php
-                    endforeach;
+                                    <section class="section--highlights__link__bottom">
+                                        <?php 
+                                            $turn_off_excerpt = get_field('turn_off_excerpt');
 
-                    wp_reset_postdata();
-                ?>
-            </ul>
+                                            if( ! $turn_off_excerpt ) {
+                                                echo '<p class="section--highlights__link__excerpt">' . get_the_excerpt();
+                                            }
+                                            
+                                            if ( has_post_thumbnail() ) {
+                                                echo '<section class="section--highlights__link__image-con">' . wp_get_attachment_image(get_post_thumbnail_id(), 'medium', false, array('class' => 'section__link__image section--highlights__link__image') ) . '</section>';
+                                            }
+                                        ?>
+                                    </section>
+                                </a>
+                            </li>
+                    <?php
+                        endforeach;
+
+                        wp_reset_postdata();
+                    ?>
+                </ul>
+
+                <section class="section section--highlights__follow">
+                    <ul class="section__posts section--highlights__follow__posts">
+                        <li class="section--highlights__follow__post"><a class="twitter-timeline" data-height="300" href="https://twitter.com/ITSUofT?ref_src=twsrc%5Etfw">Tweets by ITSUofT</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></li>
+
+                        <li class="section--highlights__follow__post"><a class="twitter-timeline" data-height="300" href="https://twitter.com/UofTCyberAware?ref_src=twsrc%5Etfw">Tweets by UofTCyberAware</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></li>
+
+                        <li class="section--highlights__follow__post"><a class="twitter-timeline" data-height="300" href="https://twitter.com/bjuul?ref_src=twsrc%5Etfw">Tweets by bjuul</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></li>
+
+                        <!-- <?php
+                            global $post;
+
+                            $args = array(
+                                'category_name' => 'Follow',
+                                'posts_per_page' => 4
+                            );
+
+                            $postslist = get_posts( $args );
+
+                            foreach ( $postslist as $post ) :
+                                setup_postdata( $post ); 
+                        ?>
+                                    
+                                <li class="section--follow__post">
+                                    <a class="section__link section--follow__link" href="<?php the_permalink(); ?>">
+                                        <button class="section--follow__button section--follow__link__button"><?php the_title(); ?></button>
+                                    </a>
+                                </li>
+                        <?php
+                            endforeach;
+
+                            wp_reset_postdata();
+                        ?> -->
+                    </ul>
+                </section>
+            </section>
         </section>
     </section>
 </main>
