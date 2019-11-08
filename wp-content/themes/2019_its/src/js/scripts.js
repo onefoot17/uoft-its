82808e1b03f7b691efc32ffba80da1ed1edb2a97
Earl Misquitta <e.misquitta@utoronto.ca>
2019-11-08 15:55:12 -0500

    Code formatting adjustments and cleanup

--- wp-content/themes/2019_its/src/js/scripts.js
+++ wp-content/themes/2019_its/src/js/scripts.js
@@ -1,4 +1,19 @@
+// Imports
+// Menu Toggle
 import {closest_polyfill} from "./_closest_polyfill.js"
+
+// Variables
+const html = document.querySelector( 'html' );
+
+// Non-touch device detection
+// first time user touches the screen
+document.addEventListener( 'touchstart', function addtouchclass( event ) {
+    // add "is-touch" class to document root using classList API
+    html.classList.add( 'is-touch' );
+
+    // de-register touchstart event
+    document.removeEventListener( 'touchstart', addtouchclass, false );
+}, false )
 
 // Glide slider carousel
 const carousels = document.querySelectorAll( '.glide' );
@@ -15,13 +30,12 @@ Object.values( carousels ).map( carousel => {
 // Menu Toggle
 closest_polyfill();
 
+
 document.addEventListener( 'click', function ( event ) {
-    const body = document.querySelector( 'body' );
-    
 	if ( event.target.closest( ( '.menu-toggle' ) ) ) {
         const type = event.type,
-        menu_toggle = event.target.closest( ( '.menu-toggle' ) ),
-        menu_main = document.querySelector( menu_toggle.getAttribute( 'data-href' ) );
+            menu_toggle = event.target.closest( ( '.menu-toggle' ) ),
+            menu_main = document.querySelector( menu_toggle.getAttribute( 'data-href' ) );
 
         // Return if key pressed was not Space Bar or Enter
         if ( type === 'keydown' && ( event.keyCode !== 13 && event.keyCode !== 32 ) ) {
@@ -35,13 +49,13 @@ document.addEventListener( 'click', function ( event ) {
             menu_main.setAttribute( 'aria-hidden', 'false' );
             menu_toggle.setAttribute( 'aria-expanded', 'true' );
             
-            body.classList.add( 'menu-is-open' );
+            html.classList.add( 'menu-open' );
         } else {
             // Menu is open
             menu_main.setAttribute( 'aria-hidden', 'true' );
             menu_toggle.setAttribute( 'aria-expanded', 'false' );
             
-            body.classList.remove( 'menu-is-open' );
+            html.classList.remove( 'menu-open' );
         }
     }
 }, false);
