8295bddf74b3373ee868ddfced9de0425c30a0c5
Earl Misquitta <e.misquitta@utoronto.ca>
2019-12-04 13:01:52 -0500

    Adding 404 page styling

--- wp-content/themes/2019_its/build/404.php
+++ wp-content/themes/2019_its/build/404.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
+
 <main id="content">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php esc_html_e( 'Not Found', 'its_2019' ); ?></h1>
-</header>
-<div class="entry-content">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'its_2019' ); ?></p>
-<?php get_search_form(); ?>
-</div>
-</article>
+    <section class="section section--category">
+        <section class="section__width section--category__width">
+        
+            <h1 class="search-not-found-title"><?php esc_html_e( '404', 'its_2019' ); ?></h1>
+            
+            <p class="search-not-found-text"><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'its_2019' ); ?></p>
+
+            <section class="search-form-container"><?php get_search_form(); ?></section>
+        </section>
+    </section>
 </main>
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
